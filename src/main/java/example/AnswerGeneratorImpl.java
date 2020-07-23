b5ca61e4aa467144a5b617d6799c43488b3fd5ee
littleDrive <2682155146@qq.com>
2020-07-24 00:34:10 +0800

    [GuessNumberGame] [zero_ou] feature: add generate test which should_return_a_valid_answer_when_call_generator_given_nothing

---
+++ src/main/java/example/AnswerGeneratorImpl.java
@@ -0,0 +1,12 @@
+package example;
+
+/**
+ * @author ozm
+ * @date 2020/7/24 - 0:07
+ */
+public class AnswerGeneratorImpl implements AnswerGenerator{
+    @Override
+    public String generator() {
+        return null;
+    }
+}
---
+++ src/test/java/example/AnswerGeneratorTest.java
@@ -0,0 +1,28 @@
+package example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+/**
+ * @author ozm
+ * @date 2020/7/24 - 0:06
+ */
+public class AnswerGeneratorTest {
+    @Test
+    void should_return_a_valid_answer_when_call_generator_given_nothing() {
+        //given
+        AnswerGenerator answerGenerator = new AnswerGeneratorImpl();
+
+        //when
+        String answer = answerGenerator.generator();
+
+        //then
+        Assertions.assertTrue( answer != null);
+        Assertions.assertEquals(4, answer.length());
+        Assertions.assertTrue(Integer.valueOf(answer.indexOf(0))<=9 && Integer.valueOf(answer.indexOf(0))>=0);
+        Assertions.assertTrue(Integer.valueOf(answer.indexOf(1))<=9 && Integer.valueOf(answer.indexOf(1))>=0);
+        Assertions.assertTrue(Integer.valueOf(answer.indexOf(2))<=9 && Integer.valueOf(answer.indexOf(2))>=0);
+        Assertions.assertTrue(Integer.valueOf(answer.indexOf(3))<=9 && Integer.valueOf(answer.indexOf(3))>=0);
+    }
+}
