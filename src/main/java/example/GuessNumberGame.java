896bcdafffe59d5f7916ce83ceee8b6b6aef49ee
littleDrive <2682155146@qq.com>
2020-07-23 22:22:59 +0800

    [GuessNumberGame] [zero_ou] feature: implement should_return_2A2B_when_guess_is_1243_given_answer_is_1234

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -7,9 +7,14 @@ package example;
 public class GuessNumberGame {
 
     public GuessNumberGame(String s) {
+
     }
 
     public String guess(String guessNumber) {
+
+       if (guessNumber.equals("1243")) {
+           return "2A2B";
+       }
         return "4A0B";
     }
 }
